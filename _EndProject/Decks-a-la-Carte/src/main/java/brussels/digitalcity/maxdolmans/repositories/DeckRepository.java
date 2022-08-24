81ad8d9c709c0fff5d40d744189664cc51ad352b
SuperSamedi <contact.max.dolmans@gmail.com>
2022-08-24 07:40:00 +0200

    déplacé des trucs

--- _EndProject/Decks-a-la-Carte/src/main/java/brussels/digitalcity/maxdolmans/repositories/DeckRepository.java
+++
@@ -1,7 +0,0 @@
-package brussels.digitalcity.maxdolmans.repositories;
-
-import brussels.digitalcity.maxdolmans.models.entities.Deck;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeckRepository extends JpaRepository<Deck, Long> {
-}
